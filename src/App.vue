6815663997e55b15e4f169ecdd4216139effca32
Ralle1968 <graef.ralf@gmail.com>
2018-03-20 16:47:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    
+    <v-toolbar>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+     
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Einkaufsliste'
+    }
+  },
+  name: 'App'
+}
+</script>
